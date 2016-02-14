75006092cf9b43a3e507ce519241eec17e41c267
Benoit Hirbec <benoit@gardentechno.com>
2016-02-13 23:16:47 -0800

    still experimenting

--- client/line-chart.js
+++ client/line-chart.js
@@ -5,13 +5,22 @@ module.exports = React.createClass({
     componentWillReceiveProps: function(props, state) {
         var el = this.getDOMNode();
         el.innerHTML = "";
-        el.appendChild(createLineChart(el, props.data, props.exchanger));
+
+        var exchangers = ['Cex', 'Kraken', 'Btce', 'Hitbtc', 'Bitfinex'];
+
+        for (var i = 0; i < exchangers.length; i++) {
+            el.appendChild(createLineChart(el, props.data, exchangers[i]));
+        }
     },
 
     render: function() {
         return <div className="chart" />
     }
 });
+
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 800 - margin.left - margin.right,
+    height = 140 - margin.top - margin.bottom;
 
 function createLineChart(el, data, exchanger) {
     var formatDate = d3.time.format("%Y-%m-%d %H:%M");
@@ -23,19 +32,19 @@ function createLineChart(el, data, exchanger) {
 
     var svgRoot = document.createElementNS(d3.ns.prefix.svg, 'svg');
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 800 - margin.left - margin.right,
-        height = 140 - margin.top - margin.bottom;
+    var svg = d3.select(svgRoot)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
 
+    var g = addLineChart(data, exchanger);
+    svgRoot.appendChild(g);
+    return svgRoot;
+}
+
+function addLineChart(data, exchanger) {
+    // x axis
     var x = d3.time.scale().range([0, width]);
-
-    var ymin = d3.min(data, function(d) { return d.Orderbooks[exchanger] ? d.Orderbooks[exchanger].Bids[0].Price: null }),
-        ymax = d3.max(data, function(d) { return d.Orderbooks[exchanger] ? d.Orderbooks[exchanger].Asks[0].Price: null }),
-        delta = (ymax - ymin) * 0.1;
-
-    var y = d3.scale.linear()
-        .domain([ymin - delta, ymax + delta])
-        .range([height, 0]);
+    x.domain(d3.extent(data, function(d) { return d.date; }));
 
     var xAxis = d3.svg
         .axis()
@@ -44,27 +53,33 @@ function createLineChart(el, data, exchanger) {
         .ticks(d3.time.minutes, 5)
         .tickFormat(d3.time.format("%H:%M"));
 
+    // y axis
+    var ymin = d3.min(data, function(d) { return d.Orderbooks[exchanger] ? d.Orderbooks[exchanger].Bids[0].Price: null }),
+        ymax = d3.max(data, function(d) { return d.Orderbooks[exchanger] ? d.Orderbooks[exchanger].Asks[0].Price: null }),
+        delta = (ymax - ymin) * 0.1;
+
+    var y = d3.scale.linear()
+        .domain([ymin - delta, ymax + delta])
+        .range([height, 0]);
+
     var yAxis = d3.svg
         .axis()
         .scale(y)
         .orient("left")
         .ticks(4);
 
-    var svg = d3.select(svgRoot)
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
+    var g = document.createElementNS(d3.ns.prefix.svg, 'g');
+
+    var container = d3
+        .select(g)
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    // y.domain(d3.extent(data, function(d) { return d.bid; }));
-
-    svg.append("g")
+    container.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
 
-    svg.append("g")
+    container.append("g")
         .attr("class", "y axis")
         .call(yAxis)
         .append("text")
@@ -74,21 +89,21 @@ function createLineChart(el, data, exchanger) {
         .style("text-anchor", "end");
         // .text("Price ($)");
 
-    function addLine(attr, color) {
+    function addLine(container, attr, color) {
         var line = d3.svg.line()
             .x(function(d) { return x(d.date); })
             .y(function(d) { return y(d.Orderbooks[exchanger] ? d.Orderbooks[exchanger][attr][0].Price : 0); });
 
-        svg.append("path")
+        container.append("path")
             .datum(data)
             .attr("class", "line")
             .attr("d", line)
             .attr("stroke", color);
     }
 
-    addLine('Bids', 'steelblue');
-    addLine('Asks', '#FC9E27');
-    return svgRoot;
+    addLine(container, 'Bids', 'steelblue');
+    addLine(container, 'Asks', '#FC9E27');
+    return g;
 }
 
 
